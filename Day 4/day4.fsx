9a0a35ab49111a0b0794467c3abafdd75c70aaca
Philip Markowski <philipmarkowski642@gmail.com>
2019-12-04 20:30:13 -0500

    Solved day 4 part 2

--- Day 4/day4.fsx
+++ Day 4/day4.fsx
@@ -12,6 +12,18 @@ let digitsContainDouble input =
     let password = string input
     Regex.IsMatch(password, @"^[0-9]*(11|22|33|44|55|66|77|88|99)+[0-9]*$")
 
+let digitsContainOneDouble input =
+    let password = string input
+    Regex.IsMatch(password, @"^(11)[2-9]*$") ||
+    Regex.IsMatch(password, @"^[1]*(22)[3-9]*$") ||
+    Regex.IsMatch(password, @"^[1-2]*(33)[4-9]*$") ||
+    Regex.IsMatch(password, @"^[1-3]*(44)[5-9]*$") ||
+    Regex.IsMatch(password, @"^[1-4]*(55)[6-9]*$") ||
+    Regex.IsMatch(password, @"^[1-5]*(66)[7-9]*$") ||
+    Regex.IsMatch(password, @"^[1-6]*(77)[8-9]*$") ||
+    Regex.IsMatch(password, @"^[1-7]*(88)[9]*$") ||
+    Regex.IsMatch(password, @"^[1-8]*(99)$")
+
 let isValidPassword input =
     let password = string input
     password.Length = 6 &&
@@ -22,3 +34,7 @@ let rec findAllValidPasswordsBetween lowerBound upperBound =
     if lowerBound = upperBound then 0
     else if isValidPassword lowerBound then (+) 1 (findAllValidPasswordsBetween (lowerBound + 1) upperBound)
     else findAllValidPasswordsBetween (lowerBound + 1) upperBound
+
+let isValidPassword2 input =
+    isValidPassword input &&
+    digitsContainOneDouble input
