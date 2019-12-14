b106deb1815d343e53a6833dc7eb9c1db84ffc42
Philip Markowski <philipmarkowski642@gmail.com>
2019-12-14 12:12:13 -0500

    Solved day 10 part 2

--- Day 10/day10.fsx
+++ Day 10/day10.fsx
@@ -5,7 +5,7 @@ let problemInputPath = Path.Combine(__SOURCE_DIRECTORY__, "input.txt")
 
 let testInputPath testNumber = Path.Combine(__SOURCE_DIRECTORY__, sprintf "test%d.txt" testNumber)
 
-type SpaceCoordinate = { X:int; Y:int; HasAsteroid:bool; }
+type SpaceCoordinate = { X:int; Y:int; HasAsteroid:bool; OriginalX:int; OriginalY:int; }
 
 let getSpaceDistance spaceCoordinate =
     abs spaceCoordinate.X + abs spaceCoordinate.Y
@@ -15,9 +15,9 @@ let stringsToSpaceCoordinates (input:string seq) : SpaceCoordinate List =
     |> Seq.mapi (fun y spaceMapLine ->
         Seq.mapi (fun x character ->
             if character = '#' then
-                { X = x; Y = y; HasAsteroid = true }
+                { X = x; Y = y; HasAsteroid = true; OriginalX = x; OriginalY = y; }
             else
-                { X = x; Y = y; HasAsteroid = false }) spaceMapLine)
+                { X = x; Y = y; HasAsteroid = false; OriginalX = x; OriginalY = y; }) spaceMapLine)
     |> Seq.concat
     |> Seq.toList
 
@@ -29,8 +29,8 @@ let convertCoordinateToRelative coordinateOrigin coordinateToConvert =
 let calculateAngleFromYAxis spaceCoordinate =
     let x = spaceCoordinate.X
     let y = spaceCoordinate.Y
-    let angleFromXAxis = atan2 (double x) (double y)
-    match angleFromXAxis with
+    let angleFromYAxis = atan2 (double x) -(double y)
+    match angleFromYAxis with
     | angle when angle < 0.0 -> (Math.PI * 2.0) + angle
     | angle -> angle
 
@@ -64,7 +64,6 @@ let removeAsteroidFromList
 
 let getNthAsteroid asteroidToHit asteroidMap : SpaceCoordinate =
     let rec getNthAsteroid (angleIndex:int) remainingAsteroids (asteroidList:(float * SpaceCoordinate list) list) =
-        printfn "At %d of %d" angleIndex asteroidList.Length
         let asteroidLaserIsPointingAt = (List.head (snd asteroidList.[angleIndex]))
         if remainingAsteroids = 1 then asteroidLaserIsPointingAt
         else
@@ -81,12 +80,10 @@ let convertCoordinateToAbsolute coordinateOrigin coordinateToConvert =
         X = coordinateToConvert.X + coordinateOrigin.X;
         Y = coordinateToConvert.Y + coordinateOrigin.Y}
 
-let solveProblem2 inputPath =
-    let asteroidToHit = 200
+let solveProblem2 inputPath asteroidToHit =
     let spaceMap = getSpaceMapFromInput inputPath |> List.where (fun spaceCoordinate -> spaceCoordinate.HasAsteroid)
-    let bestAsteroid = spaceMap |> List.maxBy (fun coordinate -> getVisibleAsteroidsFromCoordinate coordinate spaceMap)
+    let bestAsteroid = spaceMap |> List.maxBy (fun coordinate -> (getVisibleAsteroidsFromCoordinate coordinate spaceMap) |> Map.count)
     spaceMap
     |> getVisibleAsteroidsFromCoordinate bestAsteroid
     |> getNthAsteroid asteroidToHit
-    |> convertCoordinateToAbsolute bestAsteroid
-    |> (fun coord -> coord.X * 100 + coord.Y)
+    |> (fun coord -> coord.OriginalX * 100 + coord.OriginalY)
