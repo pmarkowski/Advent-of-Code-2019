279f08bba94f2ea33f7834ddbba74d71f228fdb0
Philip Markowski <philipmarkowski642@gmail.com>
2019-12-14 10:53:14 -0500

    Day 10 part 2 in progress

--- Day 10/day10.fsx
+++ Day 10/day10.fsx
@@ -7,6 +7,9 @@ let testInputPath testNumber = Path.Combine(__SOURCE_DIRECTORY__, sprintf "test%
 
 type SpaceCoordinate = { X:int; Y:int; HasAsteroid:bool; }
 
+let getSpaceDistance spaceCoordinate =
+    abs spaceCoordinate.X + abs spaceCoordinate.Y
+
 let stringsToSpaceCoordinates (input:string seq) : SpaceCoordinate List =
     input
     |> Seq.mapi (fun y spaceMapLine ->
@@ -23,17 +26,24 @@ let convertCoordinateToRelative coordinateOrigin coordinateToConvert =
         X = coordinateToConvert.X - coordinateOrigin.X;
         Y = coordinateToConvert.Y - coordinateOrigin.Y}
 
-let calculateAngle spaceCoordinate =
+let calculateAngleFromYAxis spaceCoordinate =
     let x = spaceCoordinate.X
     let y = spaceCoordinate.Y
-    atan2 (double y) (double x)
+    let angleFromXAxis = atan2 (double x) (double y)
+    match angleFromXAxis with
+    | angle when angle < 0.0 -> (Math.PI * 2.0) + angle
+    | angle -> angle
+
+let addValueToMapList key value map =
+    if Map.containsKey key map then Map.add key (List.sortBy getSpaceDistance (value::(map.[key]))) map
+    else Map.add key [value] map
 
 let getVisibleAsteroidsFromCoordinate coordinate spaceMap =
     spaceMap
     |> List.where (fun spaceCoordinate -> spaceCoordinate <> coordinate)
     |> List.map (fun spaceCoordinate -> convertCoordinateToRelative coordinate spaceCoordinate)
-    |> List.distinctBy calculateAngle
-    |> List.length
+    |> List.fold (fun asteroidAngleMap spaceCoordinate ->
+        addValueToMapList (calculateAngleFromYAxis spaceCoordinate) spaceCoordinate asteroidAngleMap) Map.empty
 
 let getSpaceMapFromInput inputPath =
     File.ReadLines inputPath |> stringsToSpaceCoordinates
@@ -41,5 +51,42 @@ let getSpaceMapFromInput inputPath =
 let solveProblem1 inputPath =
     let spaceMap = getSpaceMapFromInput inputPath |> List.where (fun spaceCoordinate -> spaceCoordinate.HasAsteroid)
     spaceMap
-    |> List.map (fun coordinate -> getVisibleAsteroidsFromCoordinate coordinate spaceMap)
+    |> List.map ((fun coordinate -> getVisibleAsteroidsFromCoordinate coordinate spaceMap) >> Map.count)
     |> List.max
+
+let removeAsteroidFromList
+    (asteroid:SpaceCoordinate)
+    (asteroidList:(float * SpaceCoordinate list) list)
+    : (float * SpaceCoordinate list) list =
+    asteroidList
+    |> List.map (fun asteroidPair -> (fst asteroidPair, List.except [asteroid] (snd asteroidPair)))
+    |> List.where (fun asteroidPair -> not (List.isEmpty (snd asteroidPair)))
+
+let getNthAsteroid asteroidToHit asteroidMap : SpaceCoordinate =
+    let rec getNthAsteroid (angleIndex:int) remainingAsteroids (asteroidList:(float * SpaceCoordinate list) list) =
+        printfn "At %d of %d" angleIndex asteroidList.Length
+        let asteroidLaserIsPointingAt = (List.head (snd asteroidList.[angleIndex]))
+        if remainingAsteroids = 1 then asteroidLaserIsPointingAt
+        else
+            let newAsteroidList = removeAsteroidFromList asteroidLaserIsPointingAt asteroidList
+            if newAsteroidList.Length < asteroidList.Length then
+                getNthAsteroid angleIndex (remainingAsteroids - 1) newAsteroidList
+            else
+                getNthAsteroid ((angleIndex + 1) % asteroidList.Length) (remainingAsteroids - 1) newAsteroidList
+
+    getNthAsteroid 0 asteroidToHit (Map.toList asteroidMap)
+
+let convertCoordinateToAbsolute coordinateOrigin coordinateToConvert =
+    {coordinateToConvert with 
+        X = coordinateToConvert.X + coordinateOrigin.X;
+        Y = coordinateToConvert.Y + coordinateOrigin.Y}
+
+let solveProblem2 inputPath =
+    let asteroidToHit = 200
+    let spaceMap = getSpaceMapFromInput inputPath |> List.where (fun spaceCoordinate -> spaceCoordinate.HasAsteroid)
+    let bestAsteroid = spaceMap |> List.maxBy (fun coordinate -> getVisibleAsteroidsFromCoordinate coordinate spaceMap)
+    spaceMap
+    |> getVisibleAsteroidsFromCoordinate bestAsteroid
+    |> getNthAsteroid asteroidToHit
+    |> convertCoordinateToAbsolute bestAsteroid
+    |> (fun coord -> coord.X * 100 + coord.Y)
