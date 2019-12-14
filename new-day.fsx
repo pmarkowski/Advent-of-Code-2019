de4b17cb563be787d37d74537b70a3b9a04b200c
Philip Markowski <philipmarkowski642@gmail.com>
2019-12-14 18:51:00 -0500

    For some reason file contents aren't being written

--- new-day.fsx
+++ new-day.fsx
@@ -5,13 +5,5 @@ let day = int (Environment.GetCommandLineArgs().[2])
 
 let newDirectory = Directory.CreateDirectory(Path.Combine(__SOURCE_DIRECTORY__, sprintf "Day %d" day))
 
-let fSharpScript = File.CreateText(Path.Combine(newDirectory.FullName, (sprintf "day%d.fsx" day)))
-fSharpScript.Write(@"
-open System
-open System.IO
-
-let problemInputPath = Path.Combine(__SOURCE_DIRECTORY__, ""input.txt"")
-let testInputPath testNumber = Path.Combine(__SOURCE_DIRECTORY__, sprintf ""test%d.txt"" testNumber)
-")
-
+let fSharpScript = File.Create(Path.Combine(newDirectory.FullName, (sprintf "day%d.fsx" day)))
 File.Create(Path.Combine(newDirectory.FullName, "input.txt"))
